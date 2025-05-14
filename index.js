7e43fb59bf1d315ec74cbac3685a93c81b05915e
Miriam. C <miriam.clemente25@compscihigh.org>
2025-05-14 16:58:17 +0000

    djgud

--- index.js
+++ index.js
@@ -15,15 +15,23 @@ app.use((req, res, next) => {
     next();
 });
 
-const sourceSchema = new mongoose.Schema({
+
+const homeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     link: {type: String, required: true },
     about: {type: String, required: true}
 });
 
-const Source = mongoose.model("Source", sourceSchema, "Sources");
+const Home = mongoose.model("Home", homeSchema, "Homes");
 
 
+const resourceSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    link: {type: String, required: true },
+    about: {type: String, required: true}
+});
+
+const  Resource = mongoose.model("Resource", resourceSchema, "Resources");
 
 const postSchema = new mongoose.Schema({
 imageLocation: {type: String, required: true},
@@ -32,15 +40,16 @@ description: {type: String, required: true},
 });
 const Post = mongoose.model("Post", postSchema, "Posts");
 
-
-
 app.get("/", async (req, res) => {
-  const sources = await Source.find({}).sort({ createdAt: -1 });
-  res.render("finds.ejs", { sources });
+  const homes = await Home.find({}).sort({ createdAt: -1 });
+  res.render("home.ejs", { homes });
 });
 
 
-//-----------------------------------
+app.get("/resources", async (req, res) => {
+    const resources = await Resource.find({});
+    res.render("finds.ejs", { resources })
+});
 
 app.get("/posts", async (req, res) => {
     const posts = await Post.find({});
@@ -50,31 +59,31 @@ app.get("/posts", async (req, res) => {
 app.delete("/posts/:_id", async (req, res) => {
     const post = await Post.findOneAndDelete({ _id: req.params._id })
     res.json(post);
-    });
+});
 
-   app.patch("/posts/:_id", async (req, res) => {
+app.patch("/posts/:_id", async (req, res) => {
     const post = await Post.findOneAndUpdate({_id: req.params._id }, 
     req.body, {new: true})
     res.json(posts);
-    });
+});
 
-     app.post("/contact/save", async (req, res) => {
+app.post("/contact/save", async (req, res) => {
      const con1 = await new User({
      user: "Mayri Clemente",
      email: "Mayrisomethinggg@gmail.com",
-     about: "shdwhhdw",
+     about: "fsfhkhdsfdgk",
      }).save()
      res.json(con1);
-     });
+});
 
-     app.post("/contact/save", async (req, res) => {
-     const con2 = await new User({
-     user: "VAW Global Health Alliances",
-     located: "8595 Pelham Rd, Suite 400 #301 Greenville, SC 29615, USA",
-     email: "teamdevelopment@vawglobal.org",
-     }).save()
-     res.json(con2);
-     });
+app.post("/contact/save", async (req, res) => {
+    const con2 = await new User({
+    user: "VAW Global Health Alliances",
+    located: "8595 Pelham Rd, Suite 400 #301 Greenville, SC 29615, USA",
+    email: "teamdevelopment@vawglobal.org",
+    }).save()
+    res.json(con2);
+});
 
 async function prepopulateDb() {
     try {
@@ -86,15 +95,13 @@ async function prepopulateDb() {
            {name: "Public Health AmeriCorps", link: "https://www.americorps.gov/serve/americorps/americorps-state-national", about: "Advance more equitable health outcomes for underserved communities with Public Health AmeriCorps."},
         ]);
         console.log('Resources added successfully!');
-        } catch (err) {
+    } catch (err) {
         console.error('Error prepopulating database:', err);
-        }
-        }
+    }
+}
 
-
-        
-        
-async function startServer() {
+      
+async function startServer(){
     // Add your SRV string, make sure that the database is called SE12
     await mongoose.connect("mongodb+srv://CSH:CSH2025@cluster0.6uo1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
@@ -106,4 +113,4 @@ async function startServer() {
     });
 }
 
-startServer();
+startServer(); 
